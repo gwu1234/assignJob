5154fca839131320f47b0e09faab4214c9fad588
Guoping Wu <mr.guoping.wu@gmail.com>
2019-01-30 16:48:07 -0500

    add new emplyee

---
+++ src/components/Messages/AddEmployeeModal.js
@@ -0,0 +1,244 @@
+import React, { Component } from 'react';
+import firebase from "../../firebase";
+import { Button, Header, Icon, Modal, Form} from 'semantic-ui-react';
+
+export default class AddEmployeeModal extends Component {
+  state = {
+    modalOpen: false,
+    lastname: '',
+    firstname: '',
+    street: '',
+    city: '',
+    postcode: '',
+    province: '',
+    country: '',
+    phone1: '',
+    phone2: '',
+    cell1: '',
+    cell2: '',
+    email1: '',
+    email2: '',
+  }
+
+
+  handleOpen = (open) => this.setState({ modalOpen: open })
+
+  handleSubmit = () => {
+    const event = this.nativeEvent;
+    if (event) {
+       //console.log(event);
+       event.preventDefault();
+    }
+    //event.preventDefault();
+    if (this.isFormValid(this.state)) {
+         //console.log("data is OK");
+         const { lastname,firstname,street,city,postcode,province,
+                 country,phone1,phone2,cell1,cell2,
+                 email1,email2} = this.state;
+         const {usertag } = this.props;
+         const name = firstname + " " + lastname;
+         let nameTag = firstname + lastname + Math.random().toString(36).substr(2, 4);
+         nameTag = (nameTag.replace(/[.,#$\[\]@ ]/g,'')).toLowerCase();
+
+         let emails = [];
+         let phones = [];
+         let cells = [];
+
+         if (email1) {
+            emails.push (email1);
+         }
+         if (email2) {
+            emails.push (email2);
+         }
+
+         if (phone1) {
+            phones.push (phone1);
+         }
+         if (phone2) {
+            phones.push (phone2);
+         }
+
+         if (cell1) {
+            cells.push (cell1);
+         }
+         if (cell2) {
+            cells.push (cell2);
+         }
+
+         const newEmployee = {
+           "city":  String(city),
+           "lastname": String (lastname),
+           "firstname": String(firstname),
+           "street": String(street),
+           "name": String(name),
+           "postcode": String(postcode),
+           "country": String(country),
+           "province":  String(province),
+           "emails": emails,
+           "phones": phones,
+           "cells": cells,
+           "tag": String(nameTag),
+         }
+         //console.log(newEmployee);
+         //console.log(nameTag);
+         const employeePath = "repos/" + usertag + "/employees/" + nameTag;
+         //console.log(employeePath);
+         const employeeRef = firebase.database().ref(employeePath);
+         //const contactKey = contactRef.push().getKey();
+         //console.log(contactPath);
+         employeeRef.set(newEmployee);
+
+         this.handleOpen(false);
+    }
+    //console.log("submit clicked");
+    //this.handleOpen(false);
+  };
+
+  isFormValid() {
+    const {lastname, firstname, street, postcode,
+           phone1, phone2, cell1, cell2}
+           = this.state;
+    if (!lastname || !firstname ){
+       window.alert("lastname and firstname are required");
+       return false;
+    }
+
+    if (!street || !postcode ){
+       window.alert("street name, number and post code are required");
+       return false;
+    }
+
+    if (!phone1 && !phone2 && !cell1 && !cell2 ){
+       window.alert("at least one phone or cell number is required");
+       return false;
+    }
+    return true;
+  }
+
+  handleChange = event => {
+    //console.log([event.target.name]);
+    //console.log(event.target.value);
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    //const {clientname, usertag, clienttag } = this.props;
+    //const {clientname} = this.props;
+    //console.log ("AddOrderModal clientname = " + clientname );
+    //console.log ("AddOrderModal usertag =" + usertag );
+    //console.log ("AddOrderModal clienttag =" + clienttag );
+
+    const titleString = "Add New Employee";
+    //console.log (titleString);
+    //const { value } = this.state
+
+    return (
+      <Modal
+        trigger={<Button icon size="mini" onClick={() => this.handleOpen(true)} style = {{position: "relative", float: "left"}}> <Icon name='plus' size ="mini"/> </Button>}
+        open={this.state.modalOpen}
+        onClose={this.handleClose}
+        basic
+        size='small'
+        style={{background: "#ccc"}}
+      >
+        <Header icon='add user' content={titleString} style = {{fontSize: "1.2em", fondStyle: "bold", color:"black"}}/>
+        <Modal.Content>
+        <Form >
+           <Form.Group inline width='equal' >
+               <Form.Input size ="small"
+                           label='First Name'
+                           placeholder='Alain'
+                           name="firstname"
+                           onChange={this.handleChange} />
+                <Form.Input size ="small"
+                            label='Last Name'
+                            placeholder='Dubois'
+                            name="lastname"
+                            onChange={this.handleChange} />
+           </Form.Group>
+           <Form.Group inline width='equal' >
+               <Form.Input size ="small"
+                           label='Street & No'
+                           placeholder='1988 Henri-Bourassa Boulevard West'
+                           name="street"
+                           onChange={this.handleChange} />
+          </Form.Group>
+           <Form.Group inline width='equal' >
+                <Form.Input size ="small"
+                            label='City'
+                            placeholder='Pointe Claire'
+                            name="city"
+                            onChange={this.handleChange} />
+                <Form.Input size ="small"
+                            label='PostCode'
+                            placeholder='H1A 1B1'
+                            name="postcode"
+                            onChange={this.handleChange} />
+           </Form.Group>
+           <Form.Group inline width='equal' >
+               <Form.Input size ="small"
+                           label='province'
+                           placeholder='Quebec'
+                           name="province"
+                           onChange={this.handleChange} />
+                <Form.Input size ="small"
+                            label='Country'
+                            placeholder='Canada'
+                            name="country"
+                            onChange={this.handleChange} />
+           </Form.Group>
+           <Form.Group inline width='equal'>
+               <Form.Input size ="small"
+                           label='Phone1'
+                           placeholder='5141119999'
+                           name="phone1"
+                           onChange={this.handleChange} />
+                <Form.Input size ="small"
+                            label='Phone2'
+                            placeholder='8003391178'
+                            name="phone2"
+                            onChange={this.handleChange} />
+           </Form.Group>
+           <Form.Group inline width='equal'>
+               <Form.Input size ="small"
+                           label='Cell 1'
+                           placeholder='5143330123'
+                           name="cell1"
+                           onChange={this.handleChange} />
+                <Form.Input size ="small"
+                            label='Cell 2'
+                            placeholder='4381112222'
+                            name="cell2"
+                            onChange={this.handleChange} />
+           </Form.Group>
+           <Form.Group inline width='equal'>
+               <Form.Input size ="small"
+                           label='email 1'
+                           placeholder='alain@hotmail.com'
+                           name="email1"
+                           onChange={this.handleChange} />
+                <Form.Input size ="small"
+                            label='email 2'
+                            placeholder='dubois@yahoo.com'
+                            name="email2"
+                            onChange={this.handleChange} />
+           </Form.Group>
+        </Form>
+        </Modal.Content>
+        <Modal.Actions>
+        <Button color="red" size="small" inverted
+              onClick={() => this.handleOpen(false)}
+              >
+              Cancel
+        </Button>
+
+          <Button color='green' size="small" inverted
+                onClick={() =>this.handleSubmit()}
+                >
+                Submit
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    )
+  }
+}
--- src/components/Messages/Employees.js
+++ src/components/Messages/Employees.js
@@ -1,8 +1,9 @@
 import React from "react";
 //import firebase from "../../firebase";
 import { connect } from "react-redux";
-import { Menu, Icon, Button} from "semantic-ui-react";
+import { Menu, Icon, Button, Dropdown} from "semantic-ui-react";
 import Employee from "./Employee";
+import AddEmployeeModal from "./AddEmployeeModal";
 import "./Employees.css";
 
 class Employees extends React.Component {
@@ -19,22 +20,63 @@ class Employees extends React.Component {
    displayEmployees = employees =>
       employees.length > 0 &&
       employees.map(employee => (
-          <Employee key={employee.name} employee={employee} />
+          <Employee key={employee.key} employee={employee.employee} />
      ));
+
+     handleEditting = () => {
+       //console.log("handle editting");
+       window.alert("handle editting");
+     };
+
+     handleAdding = () => {
+       //console.log("handle Adding");
+       window.alert("handle adding");
+     };
+
+     dropdownOptions = () => [
+       {
+         style: {height: "3.7em", border: "1px dotted brown"},
+         key: "editEmployee",
+         text: <span onClick={this.handleEditting}> Edit Employee </span>
+       },
+       {
+         style: {height: "3.7em", border: "1px dotted brown"},
+         key: "addEmployee",
+         text: <Menu.Header as="h5" > Add Employee <AddEmployeeModal usertag = {this.props.usertag} /></Menu.Header>
+       }
+     ];
 
 
   render() {
     const {employees} = this.props;
     const {display} = this.state;
 
+    const employeeArray =[];
+    for (var key in employees) {
+       const newEmployee = {
+         key: key,
+         employee: employees[key]
+       }
+       employeeArray.push(newEmployee);
+    }
+
     return (
       <Menu.Menu className="EmployeesMenuMenu" >
-            <Menu.Header as="h5" style={{textAlign:"center", top:"0em", paddingTop:'0em'}}>
-                <Button icon size="mini" onClick={() => this.onButtonClick(display)}> <Icon name='eye' size ="large"/> </Button> &nbsp; Employee &nbsp; List
-            </Menu.Header>
-          <Menu.Menu style = {this.state.employeesStyle}>
-              {display && employees && this.displayEmployees(employees)}
-          </Menu.Menu>
+      <Menu.Header as="h5" style={{textAlign:"center", top:"0em", paddingTop:'0em'}}>
+          <Button icon size="mini" onClick={() => this.onButtonClick(display)}> <Icon name='eye' size ="large"/> </Button>
+        <Dropdown
+          trigger={
+            <span style = {{color:"white"}}>
+              &nbsp; Employees List
+            </span>
+          }
+          options={this.dropdownOptions()}
+          style = {{color: "white", left: "2px" }}
+        />
+        </Menu.Header>
+        <Menu.Menu style = {this.state.employeesStyle}>
+          {display && employees && this.displayEmployees(employeeArray)}
+        </Menu.Menu>
       </Menu.Menu>
     );
   }
@@ -42,6 +84,7 @@ class Employees extends React.Component {
 
 const mapStateToProps = state => ({
      employees: state.user.employeeList,
+     usertag: state.user.usertag
    }
 );
 
