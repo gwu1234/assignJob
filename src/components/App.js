8059e2d00954200704f8850b8a6527e0b4c55802
Guoping Wu <mr.guoping.wu@gmail.com>
2019-02-17 10:32:34 -0500

    new ui

--- src/components/App.js
+++ src/components/App.js
@@ -13,40 +13,19 @@ import Background from './terra.jpg';
 //import DeliveryPanel from "./MapPanel/DeliveryPanel";
 import MapPanel from "./MapPanel/MapPanel";
 import MapContainer from './MapPanel/map/MapContainer';
+import TopPanel from './SidePanel/TopPanel';
 
 class App extends React.Component {
 
   render() {
-    /*const markers = [
-      {pos: {lat: 45.4474017, lng: -73.8300837}, name: 'Lake Shore', id: 0, status: 0},
-      {pos: {lat: 45.4524017, lng: -73.8410837}, name: 'Stillview', id: 0, status: 0},
-      {pos: {lat: 45.4608017, lng: -73.8408837}, name: 'Fairview Shopping', id: 0, status: 0},
-      {pos: {lat: 45.4574017, lng: -73.8400837}, name: 'position 5', id: 0, status: 0},
-      {pos: {lat: 45.4394017, lng: -73.8510837}, name: 'position 6', id: 0, status: 0},
-      {pos: {lat: 45.4408017, lng: -73.8608837}, name: 'position 7', id: 0, status: 0},
-      {pos: {lat: 45.4506017, lng: -73.8507837}, name: 'Bruce', id: 0, status: 0}
-    ];*/
+
 
     const {currentUser, mapView} = this.props;
     //console.log("App admin = " + admin);
 
     return (
       <Grid className ="app" style={{width: "100%", backgroundImage: `url(${Background})`}}>
-        <Grid.Row columns='equal' style={{width:"100%",height:"8vh",color:"white",backgroundColor:"blue"}}>
-            <Grid.Column style={{textAlign: "center"}}>
-               <span> user </span>
-            </Grid.Column >
-            <Grid.Column style={{textAlign: "center"}}>
-               <span> setting </span>
-            </Grid.Column>
-            <Grid.Column style={{textAlign: "center"}}>
-              <span> map </span>
-            </Grid.Column>
-            <Grid.Column style={{textAlign: "center"}}>
-               <span> Francaise </span>
-            </Grid.Column>
-        </Grid.Row>
-
+        <TopPanel/>
         <Grid.Row style={{width: "100%"}}>
 
         </Grid.Row>
