5c2592d3ba2dc8bf93fa376ee44138a38dcacdd1
Guoping Wu <mr.guoping.wu@gmail.com>
2019-02-23 20:23:14 -0500

    edit payment modal

--- src/components/MapPanel/EditPaymentModal.js
+++ src/components/MapPanel/EditPaymentModal.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import firebase from "../../firebase";
 import { connect } from "react-redux";
-import { Button, Header, Icon, Modal, Form} from 'semantic-ui-react';
+import { Button, Header, Icon, Modal, Form, Select} from 'semantic-ui-react';
 
 class EditPaymentModal extends Component {
   constructor(props) {
@@ -11,6 +11,14 @@ class EditPaymentModal extends Component {
          date: this.props.payment.date,
          amount: this.props.payment.amount,
          method: this.props.payment.method,
+         paymentId: this.props.payment.paymentId,
+         linkedInvoiceId: this.props.payment.linkedInvoiceId,
+         linkedInvoiceKey: this.props.payment.linkedInvoiceKey,
+         linkedInvoiceSubkey: this.props.payment.linkedInvoiceSubkey,
+         selectedInvoiceId: null,
+         selectedInvoiceKey: null,
+         selectInvoiceChange: false,
+         fieldChange: false,
      }
 }
 
@@ -19,35 +27,101 @@ class EditPaymentModal extends Component {
   handleOpen = (open) => this.setState({ modalOpen: open })
 
   handleSubmit = () => {
+    const {date,amount, method, paymentId,
+        selectedInvoiceId, selectedInvoiceKey,
+        linkedInvoiceId, linkedInvoiceKey, linkedInvoiceSubkey,
+        selectInvoiceChange, fieldChange} = this.state;
+    const {paymentKey, contact, usertag} = this.props;
+
     const event = this.nativeEvent;
+
     if (event) {
        //console.log(event);
        event.preventDefault();
     }
-    //event.preventDefault();
+
+    if (!selectInvoiceChange && !fieldChange) {
+        this.handleOpen(false);
+    }
+
     if (this.isFormValid()) {
-         const {date,amount, method} = this.state;
-         const {paymentKey, contact, usertag} = this.props;
-         //console.log (orderKey);
-         //console.log (contact.tag);
-         //console.log (contact.name);
-         //console.log (contact.clientKey);
 
-         let paymentPath = "repos/"+usertag+"/clients/data/"+ contact.clientTag +"/payments/" +paymentKey;
-         const paymentRef = firebase.database().ref(paymentPath);
-         //console.log(contractPath);
+         if (!selectInvoiceChange && fieldChange) {
+             let paymentPath = "repos/"+usertag+"/clients/data/"+ contact.clientTag +"/payments/" +paymentKey;
+             const paymentRef = firebase.database().ref(paymentPath);
+             //console.log(contractPath);
 
-         const newPayment = {
-           "date":  String(date),
-           "amount":  String(amount),
-           "method": String(method),
-           "paymentKey": String(paymentKey),
-           "clientKey": String(contact.clientKey),
-           "clientTag": String(contact.clientTag)
-         }
-         //console.log(newPayment);
-         paymentRef.set(newPayment);
-         this.handleOpen(false);
+             const newPayment = {
+                 "date":  String(date),
+                 "amount":  String(amount),
+                 "method": String(method),
+                 "paymentKey": String(paymentKey),
+                 "paymentId": String(paymentId),
+                 "clientKey": String(contact.clientKey),
+                 "clientTag": String(contact.clientTag)
+             }
+             //console.log(newPayment);
+             paymentRef.update(newPayment);
+             this.handleOpen(false);
+        }
+        else if (selectInvoiceChange) {
+
+          let invoiceSubkey = "";
+            // a non-null invoice selected
+          if (selectedInvoiceKey) {
+                const newPayment = {
+                   "linkedPaymentId":  paymentId,
+                   "linkedPaymentKey": paymentKey,
+                 }
+                const invoicePath = "repos/"+usertag+"/clients/data/"+ contact.clientTag
+                                    + "/invoices/" + selectedInvoiceKey +"/linkedPayments";
+                const invoiceRef = firebase.database().ref(invoicePath);
+                invoiceSubkey = invoiceRef.push().getKey();
+                invoiceRef.child(invoiceSubkey).update(newPayment);
+          }
+
+            // was linked before
+          if (linkedInvoiceKey) {
+                const nullPayment = {
+                    "linkedPaymentId":  null,
+                    "linkedPaymentKey": null,
+                }
+                const invoicePath = "repos/"+usertag+"/clients/data/"+ contact.clientTag
+                                    + "/invoices/" + selectedInvoiceKey +"/linkedPayments/linkedInvoiceSubkey";
+                const invoiceRef = firebase.database().ref(invoicePath);
+                invoiceRef.update(nullPayment);
+          }
+
+            let paymentPath = "repos/"+usertag+"/clients/data/"+ contact.clientTag +"/payments/" +paymentKey;
+            const paymentRef = firebase.database().ref(paymentPath);
+
+            const newPayment = {
+                "date":  String(date),
+                "amount":  String(amount),
+                "method": String(method),
+                "paymentKey": String(paymentKey),
+                "paymentId": String(paymentId),
+                "clientKey": String(contact.clientKey),
+                "clientTag": String(contact.clientTag),
+                "linkedInvoiceId": selectedInvoiceId,
+                "linkedInvoiceKey":selectedInvoiceKey,
+                "linkedInvoiceSubkey":invoiceSubkey,
+            }
+              //console.log(newContract);
+            paymentRef.update(newPayment);
+
+            this.setState ({
+                  linkedInvoiceId:  selectedInvoiceId,
+                  linkedInvoiceKey: selectedInvoiceKey,
+                  linkedInvoiceSubkey: invoiceSubkey,
+                  selectInvoiceChange: false,
+                  fieldChange: false,
+                  selectedInvoiceId:  null,
+                  selectedInvoiceKey: null,
+            })
+
+            this.handleOpen(false);
+        }
     }
   };
 
@@ -68,11 +142,65 @@ class EditPaymentModal extends Component {
      return true;
   }
 
+  handleSelectChange = (event: React.SyntheticEvent<HTMLDivElement>, data: any) => {
+      let selectedId = null;
+      let selectedKey = null;
+      //console.log(data.value);
+      //console.log("textContent = " + event.target.textContent);
+
+      if (data !== undefined) {
+        selectedKey = data.value;
+      }
+
+      if (event.target !== undefined) {
+        selectedId = event.target.textContent;
+      }
+
+      if (selectedKey ==="no contract" || selectedId ==="no contract"){
+          selectedId = null;
+          selectedKey = null;
+      }
+
+      this.setState({
+          selectedInvoiceId:  selectedId,
+          selectedInvoiceKey: selectedKey,
+          selectInvoiceChange: true,
+       });
+    }
+
   handleChange = event => {
     //console.log([event.target.name]);
     //console.log(event.target.value);
-    this.setState({ [event.target.name]: event.target.value });
+    this.setState({
+        [event.target.name]: event.target.value,
+        fieldChange: true,
+     });
   };
+
+  selectOptions = () => {
+       const {invoices} = this.props;
+       const {linkedInvoiceKey} = this.props.payment;
+
+       const Options = [
+         {
+            key: 1111,
+            text: <span>no invoice</span>,
+            value: "no invoice",
+         }
+       ];
+
+       for (var key in invoices) {
+          if ( key !== linkedInvoiceKey ) {
+               const option = {
+                  key: key,
+                  text: <span> {invoices[key].invoiceId} </span>,
+                  value: invoices[key].invoiceKey,
+               }
+               Options.push(option);
+          }
+       };
+       return Options;
+  }
 
   render() {
     const {payment, contact} = this.props;
@@ -119,7 +247,31 @@ class EditPaymentModal extends Component {
                             name="method"
                             onChange={this.handleChange} />
            </Form.Group>
+           <Form.Group inline width='equal' >
+                <Form.Input size ="mini"
+                            label='Payment Id'
+                            defaultValue = {payment.paymentId}
+                            name="paymentId"
+                            onChange={this.handleChange} />
+           </Form.Group>
         </Form>
+
+        {this.state.linkedInvoiceId && <h4 style={{color:"black", margin:"0px", padding:"0px", marginTop:"2em", marginBottom:"0.0em"}}>linked to invoice: {this.state.linkedContractId}</h4>}
+        {this.state.linkedInvoiceId && <h4 style={{color:"black", margin:"0px", padding:"0px", marginTop:"0em", marginBottom:"0.5em"}}>select invoice to change</h4>}
+        {!this.state.linkedInvoiceId && <h4 style={{color:"black", margin:"0px", padding:"0px", marginTop:"2em", marginBottom:"0.0em"}}>not linked to invoice </h4>}
+        {!this.state.linkedInvoiceId && <h4 style={{color:"black", margin:"0px", padding:"0px", marginTop:"0em", marginBottom:"0.5em"}}>select invoice to link</h4>}
+        <Select placeholder='Select Invoice Id'
+                name="invoices"
+                text={this.state.selectedValue}
+                style={{color:"black", fontStyle:"bold", margin:"0px", padding:"0px"}}
+                onChange={this.handleSelectChange}
+                options={this.selectOptions()}
+        />
+        {this.state.selectedInvoiceId && <h4 style={{color:"black", margin:"0px", padding:"0px", marginTop:"0.5em"}}>{this.state.selectedInvoiceId} &nbsp; selected. Click Submit to confirm</h4>}
+        {this.state.selectInvoiceChange && this.state.linkedInvoiceId && !this.state.selectedInvoiceId &&
+          <h4 style={{color:"black", margin:"0px", padding:"0px", marginTop:"0.5em"}}>Deleting link to invoice. Click Submit to confirm</h4>}
+
+
         </Modal.Content>
         <Modal.Actions>
         <Button color="red" size="small" inverted
@@ -141,6 +293,7 @@ class EditPaymentModal extends Component {
 const mapStateToProps = state => ({
      contact: state.user.clientContact,
      usertag: state.user.usertag,
+     invoices: state.user.invoices,
    }
 );
 
