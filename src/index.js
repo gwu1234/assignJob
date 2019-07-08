7fcb7b10d2f6669efac3397e95d0108d14f57e65
user <user@users-MacBook-Pro.local>
2019-07-08 13:17:51 -0400

    reloading go to login form

--- src/index.js
+++ src/index.js
@@ -38,20 +38,20 @@ class Root extends React.Component {
       if (user) {
         const creationDate = new Date (user.metadata.creationTime);
         const creationTime = creationDate.getTime()/1000;
-        //console.log ("creationTime " + creationTime) ;
-
+        console.log ("creationTime " + creationTime) ;
+        console.log(user);
         const loginDate = new Date (user.metadata.lastSignInTime);
         //console.log ("loginDate = "  + loginDate);
         const loginTime = loginDate.getTime()/1000;
-        //console.log ("loginTime " + loginTime) ;
+        console.log ("loginTime " + loginTime) ;
 
         this.props.setUser(user);
 
         // just registered
-        if ( loginTime === creationTime ){
+        if ( loginTime === creationTime ) {
            //this.props.history.push("/login");
            //this.props.clearUser();
-           //console.log ("signout and open modal ") ;
+           console.log ("loginTime === createTime ") ;
            //this.props.history.push("/modal");
            this.setState({ registering: true});
            firebase.auth().signOut()
@@ -60,18 +60,34 @@ class Root extends React.Component {
              .then(() => console.log("logout"));
 
         } else {
-           //console.log("different time stamp");
-           this.props.history.push("/");
+           console.log("different time stamp");
+           if (this.props.usertag) {
+                console.log("it is normal login process");
+                this.props.history.push("/");
+           } else {
+                console.log("browser is reloaded");
+                //this.props.history.push("/login");
+                firebase.auth().signOut()
+                   //.then(() => this.props.history.push("/modal"));
+                  //.then(() => alert("Registration Success: Login in 3 Minutes"));
+                  .then(() =>
+                       { console.log("logout");
+                         this.props.history.push("/login")
+                       });
+           }
         }
       } else {
         console.log ("no user object");
         if (this.state.registering) {
+             console.log("registering");
              this.props.history.push("/modal");
              this.setState({ registering: false});
         }
         else {
+           console.log("not registering");
            this.props.history.push("/login");
         }
+        console.log("clearUser");
         this.props.clearUser();
       }
     });
@@ -92,7 +108,8 @@ class Root extends React.Component {
 
 const mapStateFromProps = state => ({
   isLoading: state.user.isLoading,
-  modal: state.user.modal
+  modal: state.user.modal,
+  usertag: state.user.usertag
 });
 
 const RootWithAuth = withRouter(
