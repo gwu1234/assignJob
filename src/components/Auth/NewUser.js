c87672425312ce5d2acbbe194c4627eedcf894f2
user <user@users-MacBook-Pro.local>
2019-07-11 13:10:00 -0400

    more french

--- src/components/Auth/NewUser.js
+++ src/components/Auth/NewUser.js
@@ -26,9 +26,30 @@ export default class NewUser extends Component {
     const title = french? "Duty2Go Bref :" : "Duty2Go Brief :";
 
     const content = french?
-"Cette application est basée sur la base de données Google Cloud et sur la carte GPS.Il gère les clients, les prospects, les commandes, les contrats, les factures, les paiements, les livraisons, les camions.Il se compose de trois parties: port de gestion pour le bureau (Google Chrome est suggéré), application Apple et application Android pour les employés.Il prend en charge l'anglais et le français. Nous offrons un mois gratuit pour le procès.Vous pouvez commencer par enregistrer et configurer votre propre base de données,ou vous pouvez nous contacter (duty2go@gmail.com).Nous allons configurer un compte de test et une base de données pour vous."
+("Cette application est basée sur la base de données Google Cloud et sur la carte GPS. " +
+"Il gère les clients, les prospects, les commandes, les contrats, les factures, " +
+"les paiements, les livraisons, les camions. " +
+"Il se compose de trois parties: port de gestion pour le bureau (Google Chrome est suggéré), " +
+"application Apple et application Android pour les employés.Il prend en charge l'anglais et le français. " +
+"Nous offrons un mois gratuit pour le procès.Vous pouvez commencer par enregistrer et configurer " +
+"votre propre base de données,ou vous pouvez nous contacter. "+
+"Nous allons configurer un compte de test et une base de données pour vous.")
     :
-    "This application is based on Google cloud database and gps map.It manages clients, leads, workorders, contracts, invoices, payments, deliveies, trucks.It comes in three parts: management port for office desktop (Chrome is suggested), Apple app and Android app for employees.It supports both English and French. We offer a free month for trial.You can start with registering and setting up your own database,or you can contact us (duty2go@gmail.com).We will set up a testing account and database for you.";
+    "This application is based on Google cloud database and gps map.It manages clients, \
+    leads, workorders, contracts, invoices, payments, deliveies, trucks.\
+    It comes in three parts: management port for office desktop (Chrome is suggested), \
+    Apple app and Android app for employees.It supports both English and French. \
+    We offer a free month for trial.You can start with registering and setting \
+    up your own database,or you can contact us. We will set up a testing account and database for you.";
+
+const content2 = french?
+    ("Cette application fonctionne mieux pour les entreprises basées \
+     sur des maisons résidentielles, telles que l'aménagement paysager,\
+       déneigement, réparation de clôture, tonte de gazon, peinture de maison, \
+     nettoyage de maison, etc.")
+    :
+    ("This application works best for business based on residential houses, such as landscaping, \
+      snow removing, fence repairing, grass mowing, house painting, house cleaning etc.");
 
     return (
       <Modal
@@ -42,9 +63,12 @@ export default class NewUser extends Component {
         <Modal.Content style = {{fontSize: "1.2em", fontWeight: "normal", color:"black"}}>
            {content}
         </Modal.Content>
-        <Message style={{color:"green", fontSize:"1.1em", fontWeight:"bold", marginTop:"1em"}}>
+        <Message style={{color:"green", fontSize:"1.1em", fontWeight:"bold"}}>
             duty2go@gmail.com
         </Message>}
+        <Modal.Content style = {{fontSize: "1.2em", fontWeight: "normal", color:"black"}}>
+           {content2}
+        </Modal.Content>
         <Modal.Actions>
         <Button color='black' size="small"
               onClick={() =>this.handleFrench()}
