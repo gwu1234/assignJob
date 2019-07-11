c59a7ab61c564b9fd2a2a37c0cd1277c70162f38
user <user@users-MacBook-Pro.local>
2019-07-11 19:21:54 -0400

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import "firebase/storage";
 
 
   var config = {
-      apiKey: "AIzaSyC6mtYTtwmmpyD9TvDsGEUfLnuCuoI97uk",
-      authDomain: "duty2go.firebaseapp.com",
-      databaseURL: "https://duty2go.firebaseio.com",
-      projectId: "duty2go",
-      storageBucket: "",
-      messagingSenderId: "926351987271",
-      appId: "1:926351987271:web:37d33429c5fa5495"
+    apiKey: "AIzaSyC6mtYTtwmmpyD9TvDsGEUfLnuCuoI97uk",
+  authDomain: "duty2go.firebaseapp.com",
+  databaseURL: "https://duty2go.firebaseio.com",
+  projectId: "duty2go",
+  storageBucket: "duty2go.appspot.com",
+  messagingSenderId: "926351987271",
+  appId: "1:926351987271:web:37d33429c5fa5495"
   };
   firebase.initializeApp(config);
 export default firebase;
